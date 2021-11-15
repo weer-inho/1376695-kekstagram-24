5f388723d2a8d3cc8c5869e842d10dcf65b20c9b
Your Name <kzs1845@gmail.com>
2021-11-15 23:41:36 +0200

    added loading photo

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ import './form.js';
 import './scale.js';
 
 const RERENDER_DELAY = 500;
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 const body = document.querySelector('body');
+const fileChooser = body.querySelector('.img-upload__input');
+const preview = body.querySelector('.img-upload__preview img');
 const filterForm = body.querySelector('.img-filters__form');
 const picturesContainer = body.querySelector('.pictures.container');
 
@@ -34,5 +37,19 @@ function imgFilterHandler () {
   }, RERENDER_DELAY));
 }
 
+function loadPhoto () {
+  fileChooser.addEventListener('change', () => {
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+    if (matches) {
+      preview.src = URL.createObjectURL(file);
+    }
+  });
+}
+
+loadPhoto();
 renderServerPhotos();
 imgFilterHandler();
