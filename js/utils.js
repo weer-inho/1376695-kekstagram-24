99874732a8fa570e1abb610f80c09c5b0bf0685e
Your Name <kzs1845@gmail.com>
2021-11-22 04:25:43 +0200

    deleted old mock data, fixed criteria B12

--- js/utils.js
+++ js/utils.js
@@ -1,39 +1,3 @@
-import {NAMES, COMMENTS, DESCRIPTIONS} from './data.js';
-
-function getRandomNumber(min, max)  {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-
-function getRandomArrayElement(elements) {
-  return elements[_.random(0, elements.length - 1)];
-}
-
-function generateComment(firstArgument, ix) {
-  return {
-    id: ix+1,
-    avatar: `img/avatar-${ix+1}.svg`,
-    message: getRandomArrayElement(COMMENTS),
-    name: getRandomArrayElement(NAMES),
-  };
-}
-
-function getArrayOfComments (quantity) {
-  return Array(quantity).fill(null).map(generateComment);
-}
-
-function generatePhoto (firstArgument, ix) {
-  return {
-    id: ix+1,
-    url: `photos/${ix+1}.jpg`,
-    description: getRandomArrayElement(DESCRIPTIONS),
-    likes: getRandomNumber(15, 200),
-    comments: getArrayOfComments(getRandomNumber(1,20)),
-  };
-}
-
 function isEscapeKey (evt) {
   return evt.key === 'Escape' || evt.key === 'Esc';
 }
@@ -51,11 +15,6 @@ function shuffleArray (data) {
 }
 
 export {
-  getRandomNumber,
-  getRandomArrayElement,
-  generateComment,
-  getArrayOfComments,
-  generatePhoto,
   isEscapeKey,
   onEscKeyDown,
   shuffleArray
